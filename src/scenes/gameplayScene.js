06b894e3dd041a32a022681c6f7dd64d4729e29a
Phildo <Phildo211@gmail.com>
2016-04-27 13:35:01 -0500

    draggable magnet

--- src/scenes/gameplayScene.js
+++ src/scenes/gameplayScene.js
@@ -6,22 +6,33 @@ var GamePlayScene = function(game, stage)
   var canvas = canv.canvas;
   var ctx = canv.context;
 
+  var dragger;
+
   var w = 30;
   var h = 30;
 
   var vfield;
   var charges;
+  var mag;
 
   self.ready = function()
   {
-    vfield = new VecField2d(w,h)
+    dragger = new Dragger({source:stage.dispCanv.canvas});
+
+    vfield = new VecField(0,0,canv.width,canv.height,w,h)
+    mag = new Magnet(-0.1,0.,0.1,0.,vfield);
     charges = [];
-    charges[0] = new Charge(0.4,0.5,-1);
-    charges[1] = new Charge(0.6,0.5, 1);
+    charges[0] = mag.n;
+    charges[1] = mag.s;
+
+    dragger.register(mag.nhandle);
+    dragger.register(mag.shandle);
   };
 
   self.tick = function()
   {
+    dragger.flush();
+
     var index;
     var x;
     var y;
@@ -31,16 +42,16 @@ var GamePlayScene = function(game, stage)
     var r;
     var f;
     var maxlen = 10;
-    for(var i = 0; i < vfield.h; i++)
+    for(var i = 0; i < vfield.dh; i++)
     {
-      y = indexToSample(i,vfield.h);
-      for(var j = 0; j < vfield.w; j++)
+      y = vfield.yIndexToFSpace(i);
+      for(var j = 0; j < vfield.dw; j++)
       {
         index = vfield.iFor(j,i);
-        x = indexToSample(j,vfield.w);
+        x = vfield.xIndexToFSpace(j);
 
-        vfield.y.buff[index] = 0;
-        vfield.x.buff[index] = 0;
+        vfield.dy[index] = 0;
+        vfield.dx[index] = 0;
         for(var k = 0; k < charges.length; k++)
         {
           yd = y-charges[k].y;
@@ -49,19 +60,19 @@ var GamePlayScene = function(game, stage)
           if(r2 == 0) f = 0;
           else        f = charges[k].v/r2;
           r = sqrt(r2);
-          vfield.y.buff[index] += f*yd/r;
-          vfield.x.buff[index] += f*xd/r;
+          vfield.dy[index] += f*yd/r;
+          vfield.dx[index] += f*xd/r;
         }
 
         //repurposing variables- just making sure vector is < some length
-        yd = vfield.y.buff[index];
-        xd = vfield.x.buff[index];
+        yd = vfield.dy[index];
+        xd = vfield.dx[index];
         r2 = (xd*xd)+(yd*yd);
         if(r2 > maxlen*maxlen)
         {
           r = sqrt(r2);
-          vfield.y.buff[index] = yd/r*maxlen;
-          vfield.x.buff[index] = xd/r*maxlen;
+          vfield.dy[index] = yd/r*maxlen;
+          vfield.dx[index] = xd/r*maxlen;
         }
       }
     }
@@ -69,153 +80,73 @@ var GamePlayScene = function(game, stage)
 
   self.draw = function()
   {
-    var x_space;
-    var y_space;
-    var vec_length = 2;
-
-    ctx.lineWidth = 1.0;
-    ctx.strokeStyle = "#FF00FF";
-    ctx.fillStyle = "#550055";
-
-    x_space = canv.width / vfield.w;
-    y_space = canv.height / vfield.h;
-    for(var i = 0; i < vfield.h; i++)
-    {
-      for(var j = 0; j < vfield.w; j++)
-      {
-        y = y_space*i+(y_space/2);
-        x = x_space*j+(x_space/2);
-        index = vfield.iFor(j,i);
-        if(abs(vfield.x.buff[index]) > 0.1 && abs(vfield.x.buff[index]) > 0.1)
-        {
-          ctx.fillRect(x-1,y-1,2,2);
-          canv.drawLine(x,y,x+vfield.x.buff[index]*vec_length,y+vfield.y.buff[index]*vec_length);
-        }
-      }
-    }
+    vfield.draw();
+    ctx.strokeRect(mag.nhandle.x,mag.nhandle.y,mag.nhandle.w,mag.nhandle.h);
+    ctx.strokeRect(mag.shandle.x,mag.shandle.y,mag.shandle.w,mag.shandle.h);
   };
 
   self.cleanup = function()
   {
   };
 
-  //index:  0 refers to first, 1 refers to second, 0.5 refers to "the value between first and second"
-  //sample: both 0 AND 1 refer to, identically, "the value between last and first", 0.5 refers to "the value between first and last"
-  function indexToSample (i,n) { return   (i+0.5)/n;       }
-  function indexToSampleW(i,n) { return (((i+0.5)/n)+1)%1; }
-  function sampleToIndex (s,n) { return   (s*n)-0.5;       }
-  function sampleToIndexW(s,n) { return (((s*n)-0.5)+n)%n; }
-
-  //wrap inc/dec
-  function decw(i,n) { return ((i-1)+n)%n; };
-  function incw(i,n) { return (i+1)%n; };
-
-  var HeightMap = function(w,h)
+  var VecField = function(x,y,w,h,dw,dh)
   {
     var self = this;
+    self.x = x;
+    self.y = y;
     self.w = w;
     self.h = h;
-    self.buff = [];
-    for(var i = 0; i < w*h; i++) self.buff[i] = 0;
 
-    self.iFor = function(x,y) { return (y*w)+x; }
-    self.sample_index = function(x,y)
+    //"the number
+    self.xtransform = 1;
+    self.ytransform = 1;
+    if(self.h > self.w) self.ytransform = self.h/self.w;
+    if(self.w > self.h) self.xtransform = self.w/self.h;
+
+    //data
+    self.dw = dw;
+    self.dh = dh;
+    self.dx = []; for(var i = 0; i < self.dw*self.dh; i++) self.dx[i] = 0;
+    self.dy = []; for(var i = 0; i < self.dw*self.dh; i++) self.dy[i] = 0;
+    self.iFor = function(dx,dy) { return (dy*dw)+dx; }
+    //sample window is 1x1 grid aspect fit into w/h
+    self.xIndexToFSpace = function(i) { return (((i+0.5)/self.dw)-0.5)*self.xtransform; }
+    self.yIndexToFSpace = function(i) { return (((i+0.5)/self.dh)-0.5)*self.ytransform; }
+    self.xScreenToFSpace = function(x) { return (((x-self.x)/self.w)-0.5)*self.xtransform; };
+    self.yScreenToFSpace = function(y) { return (((y-self.y)/self.h)-0.5)*self.ytransform; };
+    self.xFSpaceToScreen = function(x) { return (((x/self.xtransform)+0.5)*self.w)+self.x; };
+    self.yFSpaceToScreen = function(y) { return (((y/self.ytransform)+0.5)*self.h)+self.y; };
+    //self.sampleToIndex(s,n) { return (s*n)-0.5; }
+
+    var x_space = self.w/self.dw;
+    var y_space = self.h/self.dh;
+    var vec_length = 2;
+    //temp vars for drawing
+    var x;
+    var y;
+
+    self.draw = function()
     {
-      return self.buff[self.iFor(x,y)];
+      ctx.lineWidth = 1.0;
+      ctx.strokeStyle = "#FF00FF";
+      ctx.fillStyle = "#550055";
+
+      for(var i = 0; i < self.dh; i++)
+      {
+        for(var j = 0; j < self.dw; j++)
+        {
+          y = self.y + y_space*i+(y_space/2);
+          x = self.x + x_space*j+(x_space/2);
+          index = self.iFor(j,i);
+          if(abs(self.dx[index]) > 0.1 && abs(self.dy[index]) > 0.1)
+          {
+            ctx.fillRect(x-1,y-1,2,2);
+            canv.drawLine(x,y,x+self.dx[index]*vec_length,y+self.dy[index]*vec_length);
+          }
+        }
+      }
+
     }
-    self.sample = function(x,y)
-    {
-      x = sampleToIndexW(x,self.w);
-      y = sampleToIndexW(y,self.h);
-      var low_x  = floor(x);
-      var high_x = ceil (x)%self.w;
-      var low_y  = floor(y);
-      var high_y = ceil (y)%self.h;
-
-      var tl = self.buff[self.iFor( low_x, low_y)];
-      var tr = self.buff[self.iFor(high_x, low_y)];
-      var bl = self.buff[self.iFor( low_x,high_y)];
-      var br = self.buff[self.iFor(high_x,high_y)];
-
-      var t = lerp(tl,tr,x%1);
-      var b = lerp(bl,br,x%1);
-      return lerp(t,b,y%1);
-    }
-  }
-  var VecField2d = function(w,h)
-  {
-    var self = this;
-    self.w = w;
-    self.h = h;
-    self.x = new HeightMap(w,h);
-    self.y = new HeightMap(w,h);
-
-    self.sample = function(x,y)
-    {
-      return {x:self.x.sample(x,y),y:self.y.sample(x,y)};
-    }
-    self.sampleFill = function(x,y,obj)
-    {
-      obj.x = self.x.sample(x,y);
-      obj.y = self.y.sample(x,y);
-      return obj;
-    }
-    self.samplePolar = function(x,y)
-    {
-      var x_val = self.x.sample(x,y);
-      var y_val = self.y.sample(x,y);
-
-      var ret = {dir:0,len:0};
-      ret.len = sqrt(x_val*x_val+y_val*y_val);
-      x_val /= ret.len;
-      y_val /= ret.len;
-      if(ret.len < 0.001) ret.dir = 0;
-      else ret.dir = atan2(y_val,x_val);
-
-      return ret;
-    }
-    self.samplePolarFill = function(x,y,obj)
-    {
-      var x_val = self.x.sample(x,y);
-      var y_val = self.y.sample(x,y);
-
-      obj.len = sqrt(x_val*x_val+y_val*y_val);
-      x_val /= obj.len;
-      y_val /= obj.len;
-      if(obj.len < 0.001) obj.dir = 0;
-      else obj.dir = atan2(y_val,x_val);
-
-      return obj;
-    }
-    self.polarAtIndex = function(i)
-    {
-      var x_val = self.x.data[i];
-      var y_val = self.y.data[i];
-
-      var ret = {dir:0,len:0};
-      ret.len = sqrt(x_val*x_val+y_val*y_val);
-      x_val /= ret.len;
-      y_val /= ret.len;
-      if(ret.len < 0.001) ret.dir = 0;
-      else ret.dir = atan2(y_val,x_val);
-
-      return ret;
-    }
-    self.polarAtIndexFill = function(i,obj)
-    {
-      var x_val = self.x.data[i];
-      var y_val = self.y.data[i];
-
-      obj.len = sqrt(x_val*x_val+y_val*y_val);
-      x_val /= obj.len;
-      y_val /= obj.len;
-      if(obj.len < 0.001) obj.dir = 0;
-      else obj.dir = atan2(y_val,x_val);
-
-      return obj;
-    }
-
-    self.iFor = self.x.iFor;
   }
 
   var Charge = function(x,y,v)
@@ -225,5 +156,62 @@ var GamePlayScene = function(game, stage)
     this.v = v;
   }
 
+  var Handle = function(charge,allowed_d,field)
+  {
+    var self = this;
+
+    self.w = 10;
+    self.h = 10;
+    self.x = field.xFSpaceToScreen(charge.x)-self.w/2;
+    self.y = field.yFSpaceToScreen(charge.y)-self.h/2;
+
+    self.ohandle;
+    self.ocharge;
+
+    self.dragging = false;
+    self.dragStart = function(evt)
+    {
+      if(!self.ohandle.dragging)
+        self.dragging = true;
+      self.drag(evt);
+    }
+    self.drag = function(evt)
+    {
+      if(!self.dragging) return;
+      self.x = evt.doX-self.w/2;
+      self.y = evt.doY-self.h/2;
+
+      charge.x = field.xScreenToFSpace(evt.doX);
+      charge.y = field.yScreenToFSpace(evt.doY);
+      var dx = self.ocharge.x-charge.x;
+      var dy = self.ocharge.y-charge.y;
+      var d = sqrt(dx*dx+dy*dy);
+      if(d == 0){ dx = 1; dy = 0; d = 1; }
+      self.ocharge.x = charge.x+((dx/d)*allowed_d);
+      self.ocharge.y = charge.y+((dy/d)*allowed_d);
+      self.ohandle.x = field.xFSpaceToScreen(self.ocharge.x)-self.ohandle.w/2;
+      self.ohandle.y = field.yFSpaceToScreen(self.ocharge.y)-self.ohandle.h/2;
+    }
+    self.dragFinish = function()
+    {
+      self.dragging = false;
+    }
+  }
+  var Magnet = function(nx,ny,sx,sy,field)
+  {
+    var self = this;
+    var dx = nx-sx;
+    var dy = ny-sy;
+    var d = sqrt(dx*dx+dy*dy);
+    self.n = new Charge(nx,ny,-1);
+    self.s = new Charge(sx,sy, 1);
+
+    self.nhandle = new Handle(self.n,d,field);
+    self.shandle = new Handle(self.s,d,field);
+    self.nhandle.ocharge = self.s;
+    self.nhandle.ohandle = self.shandle;
+    self.shandle.ocharge = self.n;
+    self.shandle.ohandle = self.nhandle;
+  }
 };
 
